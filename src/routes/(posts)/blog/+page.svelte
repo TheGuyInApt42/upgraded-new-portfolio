2e906d24496442108729c198fd6ec94b832ffb81
Ralph Jarrod Gorham <35537756+TheGuyInApt42@users.noreply.github.com>
2024-09-21 13:47:03 -0400

    updated calling postsList component

--- src/routes/(posts)/blog/+page.svelte
+++ src/routes/(posts)/blog/+page.svelte
@@ -1,25 +1,21 @@
 <script>
-	import PostsList from '$lib/components/posts-list/PostsList.svelte'
-	import Pagination from '$lib/components/pagination/Pagination.svelte'
-	import { siteDescription } from '$lib/config'
+	import PostsList from '@components/posts-list/PostsList.svelte';
+	import Pagination from '@components/pagination/Pagination.svelte';
+	import { siteDescription } from '$lib/config';
 
-	export let data
+	export let data;
 </script>
-
 
 <svelte:head>
 	<title>Blog</title>
-	<meta data-key="description" name="description" content={siteDescription}>
+	<meta data-key="description" name="description" content={siteDescription} />
 </svelte:head>
 
 <div class="flex justify-between mb-12">
-	<h1> Blog </h1>
+	<h1>Blog</h1>
 
 	<a href="/blog/category/" class="dark:text-white font-semibold">Keywords</a>
 </div>
-
-
-
 
 <PostsList posts={data.posts} />
 
--- src/routes/(posts)/blog/category/[category]/+page.svelte
+++ src/routes/(posts)/blog/category/[category]/+page.svelte
@@ -1,27 +1,25 @@
 <!-- Renders any page at /blog/category/* -->
 <script>
-	import PostsList from '$lib/components/posts-list/PostsList.svelte'
-	import Pagination from '$lib/components/pagination/Pagination.svelte'
-  import { postsPerPage } from '$lib/config'
+	import PostsList from '@components/posts-list/PostsList.svelte';
+	import Pagination from '@components/pagination/Pagination.svelte';
+	import { postsPerPage } from '$lib/config';
 
-	export let data
+	export let data;
 
-  const { page, posts, category, total } = data
+	const { page, posts, category, total } = data;
 
-	$: lowerBound = (page * postsPerPage) - (postsPerPage - 1) || 1
-	$: upperBound = Math.min(page * postsPerPage, total)
+	$: lowerBound = page * postsPerPage - (postsPerPage - 1) || 1;
+	$: upperBound = Math.min(page * postsPerPage, total);
 </script>
-
 
 <svelte:head>
 	<title>Category: {category}</title>
 </svelte:head>
 
-
 <h1 class="font-bold text-3xl md:text-5xl tracking-tight mb-4 dark:text-white">#{category}</h1>
 
 {#if posts.length}
-	<PostsList posts={posts} />
+	<PostsList {posts} />
 	<Pagination currentPage={page} totalPosts={total} path="/blog/category/{category}/page" />
 {:else}
 	<p><strong>Ope!</strong> Sorry, couldn't find any posts in the category "{category}".</p>
