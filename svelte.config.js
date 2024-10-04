2609f3dc449f78ccfee07f3bdeffd8b555688dea
Ralph Jarrod Gorham <35537756+TheGuyInApt42@users.noreply.github.com>
2024-10-04 15:18:42 -0400

    added wrangler and cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { mdsvex } from 'mdsvex';
 import preprocess from 'svelte-preprocess';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
@@ -11,7 +12,19 @@ const config = {
 	extensions: ['.svelte', '.md'],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		}),
 		alias: {
 			'@components': './src/lib/components',
 			'@stores': './src/stores'
